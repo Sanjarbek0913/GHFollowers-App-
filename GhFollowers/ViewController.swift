9e39f8b6ee75ae6832d401c2ede9ac4727bdcfb3
Sanjarbek <sanjarbekabdurayimov4@gmail.com>
2024-01-22 20:02:34 +0500

    Initial Commit

---
+++ GhFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhFollowers
+//
+//  Created by Sanjarbek Abdurayimov on 22/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
